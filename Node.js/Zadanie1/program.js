357a2a386b0278c035997cbffffeaf917008bf8d
Adam Dziedzic <adamdziedzic87@gmail.com>
2017-07-05 02:52:14 +0200

    Node.js - zadanie 1
    Zadanie: Badamy środowisko!

---
+++ Node.js/Zadanie1/program.js
@@ -0,0 +1,28 @@
+process.stdin.setEncoding('utf-8');
+
+var user = process.env['USERNAME'] || 'stranger';
+process.stdout.write('Hello ' + user + '! Type \'/help\' to see list of all supported commands.\n');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit': 
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/version':
+                process.stdout.write('Node version: ' + process.version + '\n');
+                break;
+            case '/language': 
+                process.stdout.write('System language: ' + process.env['LANG'] + '\n');
+                break;
+            case '/help':
+                process.stdout.write('/exit - quit the app\n/version - get node version\n/language - get system language\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});

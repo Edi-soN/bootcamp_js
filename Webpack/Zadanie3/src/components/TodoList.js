39815095cc2176c4a9ccfe917425fb5a1fbf8881
Adam Dziedzic <adamdziedzic87@gmail.com>
2017-07-26 20:29:12 +0200

    Webpack - zadanie 3
    Zadanie: Rozwój aplikacji Todo

---
+++ Webpack/Zadanie3/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './TodoList.css';
+
+const TodoList = props => {
+    return(
+        <div> 
+            <ul className={style.TodoList}>
+                {props.list.map((todo, i) => <li key={todo.id} onClick={() => props.remove(todo.id)}>{todo.text}</li>)}
+            </ul>
+        </div>
+)}
+
+export default TodoList;

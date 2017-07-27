42f691204e1784a208f1575b96bebc89cb5cf187
Adam Dziedzic <adamdziedzic87@gmail.com>
2017-07-27 19:38:08 +0200

    Webpack - zadanie 4
    Zadanie: Dokończ tworzenie aplikacji to-do

--- Webpack/Zadanie4/src/components/Title.js
+++ Webpack/Zadanie4/src/components/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import style from './Title.css';
 
-const Title = props => <h1>{props.title} ({props.quantity})</h1>;
+const Title = props => <h1>{props.title} <span className={style.Title}>({props.quantity})</span></h1>;
 
 export default Title;

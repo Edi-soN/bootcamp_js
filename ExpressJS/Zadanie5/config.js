29ab2b5b34a7e4f967317a319ad7c924e2596242
Adam Dziedzic <adamdziedzic87@gmail.com>
2017-08-03 20:42:08 +0200

    ExpressJS - zadanie 5
    Zadanie: Aplikacja z wykorzystaniem autoryzacji Google

---
+++ ExpressJS/Zadanie5/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '91496026863-44u31q6bboq76ivj7v83926boe1f9hh2.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'Iai2HSiL5nbhK6csIAFAJksl',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}

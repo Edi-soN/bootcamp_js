b6651462aaedc8e2716e5b63fda9f20b2e7a3cec
Adam Dziedzic <adamdziedzic87@gmail.com>
2017-08-15 11:27:58 +0200

    Czat - projekt
    Zadanie: Projekt - czat

---
+++ Czat/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+const UsersService = require('./UsersService');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const userService = new UsersService();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+io.on('connection', function(socket) {
+    // klient nasłuchuje na wiadomość wejścia do czatu
+    socket.on("join", function(name){
+        // użytkownika, który pojawił się w aplikacji zapisujemy do serwisu trzymającego listę osób w czacie
+        userService.addUser({
+            id: socket.id,
+            name
+        });
+        // aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+        io.emit('update', {
+            users: userService.getAllUsers()
+        });
+    });
+    socket.on('disconnect', () => {
+        userService.removeUser(socket.id);
+        socket.broadcast.emit('update', {
+            users: userService.getAllUsers()
+        });
+    });
+    socket.on('message', function(message){
+        const {name} = userService.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text: message.text,
+            from: name
+        });
+    });
+});
+
+server.listen(8080, function(){
+  console.log('listening on *:8080');
+});
